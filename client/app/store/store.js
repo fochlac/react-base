9b6277668f86558b9f35c8f64e019f6edbec3990
Florian Riedel <f.riedel@epages.com>
2018-01-18 14:15:58 +0100

    initial store

--- client/app/store/store.js
+++ client/app/store/store.js
@@ -1,7 +1,49 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers.js';
 
-const defaultStore = window.defaultStore ? window.defaultStore : { };
+const defaultStore = window.defaultStore ? window.defaultStore : {
+	users: {
+		0: {
+			id: 0,
+			name: 'Hinz',
+			posts: [0],
+			comments: [],
+		},
+		1: {
+			id: 1,
+			name: 'Kunz',
+			posts: [],
+			comments: [0],
+
+		}
+	},
+	posts: {
+		0: {
+			id: 0,
+			user: 0,
+			title: 'Dolle Wuasd',
+			text: 'Rippenbiest und Hammelswade',
+			date: 1234567898776,
+			comments: [0],
+		}
+	},
+	comments: {
+		0: {
+			id: 0,
+			user: 1,
+			text: 'Schmeckt gut!',
+			post: 0,
+			date: 12345567891011,
+		}
+	},
+	app: {
+		ĺogin: true,
+	},
+	user: {
+		admin: true,
+		id: 0,
+	}
+};
 
 export function configureStore(initialState = {}) {
   const store = createStore(
